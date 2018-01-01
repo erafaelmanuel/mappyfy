88d7ee6326d89bad7b0501d58aaa84715f5f7c5c
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-01 17:27:21 +0800

    Create a simple test cases.

--- test/src/main/java/io/ermdev/mapfierj/test/cases/IntegerDoubleConverterTest.java
+++ test/src/main/java/io/ermdev/mapfierj/test/cases/IntegerDoubleConverterTest.java
@@ -1,4 +1,22 @@
 package io.ermdev.mapfierj.test.cases;
 
+import io.ermdev.mapfierj.typeconverter.IntegerDoubleConverter;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class IntegerDoubleConverterTest {
+
+    @Test
+    public void convertTo() {
+        int from = 1;
+        double to = 1D;
+        Assert.assertEquals((double) new IntegerDoubleConverter().convertTo(from), to, 0.00);
+    }
+
+    @Test
+    public void convertFrom() {
+        double from = 1D;
+        int to = 1;
+        Assert.assertEquals((int) new IntegerDoubleConverter().convertFrom(from), to);
+    }
 }
