4e988eec5c8ed32149985054bbb4519d4392662d
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-18 21:51:10 +0800

    Start working on sample 3.

---
+++ sample/src/main/java/io/ermdev/mapfierj/sample/sample3/Person.java
@@ -0,0 +1,14 @@
+package io.ermdev.mapfierj.sample.sample3;
+
+public class Person {
+
+    String name;
+    int age;
+    int carId;
+
+    public Person(String name, int age, int carId) {
+        this.name = name;
+        this.age = age;
+        this.carId = carId;
+    }
+}
