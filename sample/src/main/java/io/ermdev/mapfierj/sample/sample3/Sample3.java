b657965d8930bd84e68f71da49c43fc447b219a2
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-19 22:15:19 +0800

    Create a sample 3 test.

---
+++ sample/src/main/java/io/ermdev/mapfierj/sample/sample3/Sample3.java
@@ -0,0 +1,26 @@
+package io.ermdev.mapfierj.sample.sample3;
+
+import io.ermdev.mapfierj.SimpleMapper;
+import io.ermdev.mapfierj.sample.sample2.Person;
+import io.ermdev.mapfierj.sample.sample2.PersonDto;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+public class Sample3 {
+
+    Logger logger = Logger.getLogger(Sample3.class.getSimpleName());
+
+    @Test
+    public void simpleMapper() {
+        int carId = (int) (Math.random() * 10) + 1;
+
+        SimpleMapper mapper = new SimpleMapper();
+
+        Person person = new Person("Rafael", 18, carId);
+
+        PersonDto personDto = mapper.set(person).mapTo(PersonDto.class);
+
+        logger.info(personDto.toString());
+    }
+}
