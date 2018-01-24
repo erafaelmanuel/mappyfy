f7ec346f683b4e48a9c1bf35d480ba4b8cf824cb
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-24 22:30:10 +0800

    Create a CharIntegerConverter

---
+++ typeconverter/src/main/java/io/ermdev/mapfierj/typeconverter/v2/CharIntegerConverter.java
@@ -0,0 +1,28 @@
+package io.ermdev.mapfierj.typeconverter.v2;
+
+import io.ermdev.mapfierj.TypeConverter;
+import io.ermdev.mapfierj.TypeException;
+import io.ermdev.mapfierj.v2.TypeConverterAdapter;
+
+@TypeConverter
+public class CharIntegerConverter extends TypeConverterAdapter<Character, Integer> {
+
+    @Override
+    public Integer convertTo(Character o) throws TypeException {
+        try {
+            return (int) o;
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+    }
+
+    @Override
+    public Character convertFrom(Integer o) throws TypeException {
+        try {
+            return (char) (o + '0');
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+
+    }
+}
