4efff0e756548d5514b104cfbbf29cf80fd875d5
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-24 22:28:23 +0800

    Create a ByteIntegerConverter

---
+++ typeconverter/src/main/java/io/ermdev/mapfierj/typeconverter/v2/ByteIntegerConverter.java
@@ -0,0 +1,27 @@
+package io.ermdev.mapfierj.typeconverter.v2;
+
+import io.ermdev.mapfierj.TypeConverter;
+import io.ermdev.mapfierj.TypeException;
+import io.ermdev.mapfierj.v2.TypeConverterAdapter;
+
+@TypeConverter
+public class ByteIntegerConverter extends TypeConverterAdapter<Byte, Integer> {
+
+    @Override
+    public Integer convertTo(Byte o) throws TypeException {
+        try {
+            return o.intValue();
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+    }
+
+    @Override
+    public Byte convertFrom(Integer o) throws TypeException {
+        try {
+            return o.byteValue();
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+    }
+}
