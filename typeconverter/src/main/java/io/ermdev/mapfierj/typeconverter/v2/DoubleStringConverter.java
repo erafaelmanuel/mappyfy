09302e093808dd1f832ed4af247ae265f3f059e1
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-25 01:51:35 +0800

    Create a DoubleStringConverter

---
+++ typeconverter/src/main/java/io/ermdev/mapfierj/typeconverter/v2/DoubleStringConverter.java
@@ -0,0 +1,27 @@
+package io.ermdev.mapfierj.typeconverter.v2;
+
+import io.ermdev.mapfierj.TypeConverter;
+import io.ermdev.mapfierj.TypeException;
+import io.ermdev.mapfierj.v2.TypeConverterAdapter;
+
+@TypeConverter
+public class DoubleStringConverter extends TypeConverterAdapter<Double, String> {
+
+    @Override
+    public String convertTo(Double o) throws TypeException {
+        try {
+            return String.valueOf(o);
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+    }
+
+    @Override
+    public Double convertFrom(String o) throws TypeException {
+        try {
+            return Double.parseDouble(o);
+        } catch (Exception e) {
+            throw new TypeException("Failed to convert");
+        }
+    }
+}
