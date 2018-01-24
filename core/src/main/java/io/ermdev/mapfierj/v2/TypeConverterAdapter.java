5becf774c479b63cfeb14a72aba85fc2b9cee864
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-24 22:17:48 +0800

    Update the 2nd version of TypeConverterAdapter

--- core/src/main/java/io/ermdev/mapfierj/v2/TypeConverterAdapter.java
+++ core/src/main/java/io/ermdev/mapfierj/v2/TypeConverterAdapter.java
@@ -9,21 +9,19 @@ public abstract class TypeConverterAdapter<A, B> {
 
     @SuppressWarnings("unchecked")
     public <T> T convert(Object o) throws TypeException {
-        if(o != null) {
+        if (o != null) {
             Type types[];
             try {
                 types = (((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments());
+                if (types[0].equals(o.getClass())) {
+                    return (T) convertTo((A) o);
+                } else if (types[1].equals(o.getClass())) {
+                    return (T) convertFrom((B) o);
+                } else {
+                    throw new TypeException("Invalid Type");
+                }
             } catch (ClassCastException e) {
-                types = new Type[2];
-                types[0] = Object.class;
-                types[1] = Object.class;
-            }
-            if (types[0].equals(o.getClass())) {
-                return (T) convertTo((A) o);
-            } else if (types[1].equals(o.getClass())) {
-                return (T) convertFrom((B) o);
-            } else {
-                throw new TypeException("Invalid Type");
+                throw new TypeException("Failed to get the types");
             }
         } else {
             throw new TypeException("You can't convert a null object");
