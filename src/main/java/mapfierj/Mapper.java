5a075e0e058add020dff5153ab1d25f946da3040
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-15 00:24:31 +0800

    Update Mapper

--- src/main/java/mapfierj/Mapper.java
+++ src/main/java/mapfierj/Mapper.java
@@ -1,32 +1,84 @@
 package mapfierj;
 
-import java.util.Collection;
-import java.util.HashMap;
-
 public class Mapper {
 
-    private ModelMapper modelMapper;
-
-    public Mapper() {
-        modelMapper = new ModelMapper();
+    public Session set(Object o) {
+        return new Session(o);
     }
 
-    public Mapper(String packagesToScan) {
-        modelMapper = new ModelMapper(packagesToScan);
-    }
+    public class Session {
 
-    public ModelMapper set(Object o) {
-        modelMapper.set(o);
-        return modelMapper;
-    }
+        private Transaction transaction;
+        private Converter converter = new Converter();
 
-    public ModelMapper set(Collection o) {
-        modelMapper.set(o);
-        return modelMapper;
-    }
+        public Session(Object o) {
+            try {
+                transaction = new Transaction(o);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-    public ModelMapper set(HashMap<String, Object> o) {
-        modelMapper.set(o);
-        return modelMapper;
+        public Session exclude(String f) {
+            transaction.getMap().remove(f);
+            return this;
+        }
+
+        public Session excludeAll(String f) {
+            transaction.getExcludedField().add(f);
+            return this;
+        }
+
+        public Session convertField(String f, Class<?> t) {
+            try {
+                final Object o = converter.convertTo(transaction.getMap().get(f), t);
+                if (o != null) {
+                    transaction.getMap().put(f, o);
+                } else {
+                    transaction.getMap().remove(f);
+                }
+            } catch (MappingException e) {
+                e.printStackTrace();
+            }
+            return this;
+        }
+
+        public Session convertField(String f, TypeConverterAdapter c) {
+            final Object o = transaction.getMap().get(f);
+            transaction.getMap().remove(f);
+            if (o != null) {
+                try {
+                    Object instance;
+                    Converter.Session session = converter.openSession();
+                    session.set(o);
+                    session.adapter(c);
+
+                    if ((instance = session.convert()) != null) {
+                        transaction.getMap().put(f, instance);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return this;
+        }
+
+        public Session field(String f1, String f2) {
+            Object o = transaction.getMap().get(f1);
+            transaction.getMap().remove(f1);
+            if (o != null) {
+                transaction.getMap().put(f2, o);
+            }
+            return this;
+        }
+
+        public <T> T mapTo(Class<T> c) {
+            T o = transaction.mapTo(c);
+            if(o != null) {
+                return o;
+            } else {
+                return transaction.mapAllTo(c);
+            }
+        }
     }
 }
