bf4e7651b99648584d71ce7eb7702a5ec3d951dd
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-14 23:51:47 +0800

    Update Mapper class

--- src/main/java/mapfierj/Mapper.java
+++ src/main/java/mapfierj/Mapper.java
@@ -2,140 +2,31 @@ package mapfierj;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 
 public class Mapper {
 
-    private Transaction transaction;
-    private HashMap<String, Object> map = new HashMap<>();
-    private final Converter converter;
+    private ModelMapper modelMapper;
 
     public Mapper() {
-        converter = new Converter();
+        modelMapper = new ModelMapper();
     }
 
-    public Mapper(final String... scanPackages) {
-        this();
-        converter.scanPackages(scanPackages);
+    public Mapper(String packagesToScan) {
+        modelMapper = new ModelMapper(packagesToScan);
     }
 
-    public Mapper set(Object o) {
-        try {
-            transaction = new Transaction(o);
-            map = transaction.getMap();
-            return this;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public ModelMapper set(Object o) {
+        modelMapper.set(o);
+        return modelMapper;
     }
 
-    public Mapper set(HashMap<String, Object> o) {
-        try {
-            transaction = new Transaction(o);
-            map = transaction.getMap();
-            return this;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public ModelMapper set(Collection o) {
+        modelMapper.set(o);
+        return modelMapper;
     }
 
-    public Mapper set(Collection o) {
-        transaction = new Transaction(o);
-        map = transaction.getMap();
-        return this;
-    }
-
-    public Mapper field(String field, String field2) {
-        final Object o = map.get(field);
-        map.remove(field);
-        if (o != null) {
-            map.put(field2, o);
-        }
-        return this;
-    }
-
-    public Mapper exclude(String field) {
-        map.remove(field);
-        return this;
-    }
-
-    public Mapper excludeAll(String field) {
-        transaction.getExcludedField().add(field);
-        return this;
-    }
-
-    public Mapper convertFieldToType(String field, Class<?> type) {
-        try {
-            final Object instance = converter.convertTo(map.get(field), type);
-            if (instance != null)
-                map.put(field, instance);
-            else
-                map.remove(field);
-        } catch (MappingException e) {
-            e.printStackTrace();
-        }
-        return this;
-    }
-
-    public Mapper convertFieldByConverter(String field, TypeConverterAdapter adapter) {
-        final Object o = map.get(field);
-        map.remove(field);
-        if (o != null) {
-            try {
-                final Converter.Session session = converter.openSession();
-                session.set(o);
-                session.adapter(adapter);
-
-                Object instance = session.convert();
-                if (instance != null) {
-                    map.put(field, instance);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return this;
-    }
-
-    public Mapper convertFieldByConverter(String field, Class<? extends TypeConverterAdapter> adapter) {
-        final Object o = map.get(field);
-        map.remove(field);
-        if (o != null) {
-            try {
-                final Converter.Session session = converter.openSession();
-                session.set(o);
-                session.adapter(adapter);
-
-                final Object instance = session.convert();
-                if (instance != null) {
-                    map.put(field, instance);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return this;
-    }
-
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
-    public Converter getConverter() {
-        return converter;
-    }
-
-    public <T> T mapTo(Class<T> o) {
-        try {
-            return getTransaction().mapTo(o);
-        } catch (Exception e) {
-            return getTransaction().mapAllTo(o);
-        }
-    }
-
-    public <T> List<T> mapToList(Class<T> o) {
-        return getTransaction().mapToList(o);
+    public ModelMapper set(HashMap<String, Object> o) {
+        modelMapper.set(o);
+        return modelMapper;
     }
 }
