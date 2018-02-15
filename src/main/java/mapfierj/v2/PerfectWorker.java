9d3387b865adb5dda380697199981b20556113c1
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-15 13:48:33 +0800

    Create PerfectWorker

---
+++ src/main/java/mapfierj/v2/PerfectWorker.java
@@ -0,0 +1,27 @@
+package mapfierj.v2;
+
+import java.util.List;
+import java.util.Set;
+
+public class PerfectWorker extends Transactional {
+
+    @Override
+    public <T> T mapTo(Class<T> c) {
+        return null;
+    }
+
+    @Override
+    public <T> List<T> mapToList(Class<T> c) {
+        return null;
+    }
+
+    @Override
+    public <T> Set<T> mapToSet(Class<T> c) {
+        return null;
+    }
+
+    @Override
+    public <T> T[] mapToArray(Class<T> c) {
+        return null;
+    }
+}
