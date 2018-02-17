31b68b967109750da1cae62e3873fb2f05059a1d
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-17 18:45:59 +0800

    Create new Converter

---
+++ src/main/java/mapfierj/xyz/Converter.java
@@ -0,0 +1,15 @@
+package mapfierj.xyz;
+
+import mapfierj.TypeConverterAdapter;
+
+public class Converter {
+
+    public Session setAdapter(TypeConverterAdapter adapter) {
+        return new Session();
+    }
+
+    public class Session {
+
+
+    }
+}
